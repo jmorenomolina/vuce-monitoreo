ac0b3c221f7170d48476303043a45e8654bf28ca
jmoreno <jmoreno@crossnet.ws>
2018-07-27 05:08:09 -0500

    no message

---
+++ servicio-alertas-vuce/servicio-alertas-vuce/src/main/java/pe/gob/vuce/servicioalertasvuce/ServicioAlertasVuceApplication.java
@@ -0,0 +1,12 @@
+package pe.gob.vuce.servicioalertasvuce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioAlertasVuceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioAlertasVuceApplication.class, args);
+	}
+}
