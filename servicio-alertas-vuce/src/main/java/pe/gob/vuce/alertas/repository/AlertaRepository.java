085e875803a01ab2c5b25360b3ff78385f4a22b7
jmoreno <jmoreno@crossnet.ws>
2018-07-31 12:03:48 -0500

    no message

--- servicio-alertas-vuce/src/main/java/pe/gob/vuce/alertas/repository/AlertaRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.gob.vuce.alertas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pe.gob.vuce.alertas.entity.Alerta;
-
-public interface AlertaRepository extends JpaRepository<Alerta, Long>{
-}
