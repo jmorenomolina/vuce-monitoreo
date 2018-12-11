f2001e002fcdee312742983077bae56cb603227a
Cesar Quevedo <cesar.quevedo.vega@gmail.com>
2018-12-10 22:02:34 -0500

    conslola app

--- vuce-app/src/main/java/vuce/gob/pe/app/ConsolaMonitoreoApplication.java
+++ vuce-app/src/main/java/vuce/gob/pe/app/ConsolaMonitoreoApplication.java
@@ -2,12 +2,20 @@ package vuce.gob.pe.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 public class ConsolaMonitoreoApplication extends SpringBootServletInitializer {
 
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConsolaMonitoreoApplication.class);
+	}
+	
+	
     public static void main(String[] args) {
         SpringApplication.run(ConsolaMonitoreoApplication.class, args);
     }
