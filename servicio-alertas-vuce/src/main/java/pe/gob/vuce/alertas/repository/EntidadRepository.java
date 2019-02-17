8fa4b0f6f78124f60a30b5cc9ba9bcf5ec9cd101
Joel Moreno <jmorenomolina@gmail.com>
2019-02-17 14:20:44 -0500

    borrar

--- servicio-alertas-vuce/src/main/java/pe/gob/vuce/alertas/repository/EntidadRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.gob.vuce.alertas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pe.gob.vuce.alertas.entity.Entidad;
-
-public interface EntidadRepository extends JpaRepository<Entidad,Long> {
-}
